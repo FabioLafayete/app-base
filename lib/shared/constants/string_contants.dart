6a4ab3ddf5c1b4be38488339e028803f50f72dcd
fabio.g.pathbit <fabio.g.pathbit@genial.com.vc>
2024-03-07 16:18:06 -0300

    fix deep link

--- lib/shared/constants/string_contants.dart
+++ lib/shared/constants/string_contants.dart
@@ -1,8 +1,8 @@
 class StringConstants {
   const StringConstants._();
   static const String hostKey = 'host';
-  static const String hostDevEndpoint = 'https://api-dev.ibetter.site';
-  static const String hostProdEndpoint = 'https://api.ibetter.site';
+  static const String hostDevEndpoint = 'https://api-dev.ibetter.io';
+  static const String hostProdEndpoint = 'https://api.ibetter.io';
   static const String hostLocalEndpoint = 'http://127.0.0.1:3000';
   static const String devKey = 'dev';
   static const String prdKey = 'prd';
